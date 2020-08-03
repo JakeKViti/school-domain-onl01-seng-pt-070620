e34efda143cba484efc32c95b54c306b13ec0418
Jacob Viti <thevitij64@gmail.com>
2020-08-03 19:27:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,7 @@ class School
   end
 
   def add_student(student, grade)
+<<<<<<< HEAD
     if !roster.include?(grade)
     roster[grade] = []
   end
@@ -24,6 +25,17 @@ class School
       sortedHash[grade] = student.sort
     end
     return sortedHash
+=======
+    @roster = {grade}
+  end
+
+  def grade
+
+  end
+
+  def sort
+
+>>>>>>> 0fac373d850d9d7d0b188d9f9508741f06840f71
   end
 
 end
